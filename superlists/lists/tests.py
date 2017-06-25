5fa6b2db4bb4b444720c17227b5eeaaf1cf7b9b8
ym4199 <ym4199@gmail.com>
2017-06-25 13:58:09 +0900

    최소 html을 반환하는 기본 뷰

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.test import TestCase
-from django.urls import resolve
+from django.core.urlresolvers import resolve
+
 from lists.views import home_page
 
 
@@ -14,3 +16,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpResponse()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
