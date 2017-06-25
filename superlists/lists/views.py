4b1cebe07dac6dd05464fecbd1308580ffd45edc
ym4199 <ym4199@gmail.com>
2017-06-26 00:15:08 +0900

    템플릿에 있는 아이템 렌더링

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -16,7 +16,9 @@ def home_page(request):
 
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-    return render(request, 'home.html')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items':items})
     # else:
     #     new_item_text = ''
     #
