49a1353090b58893db862dc54b1adb695c6238d5
ym4199 <ym4199@gmail.com>
2017-06-25 18:45:39 +0900

    add assertIn 2 in ft

--- functional_tests.py
+++ functional_tests.py
@@ -33,13 +33,13 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
 
-        inputbox.send_keys('Buy peacock feathers')
-
+        # inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use Peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # import time
@@ -48,13 +48,18 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
+
         # self.assertTrue(
         #     any(row.text == '1:Buy peacock feathers' for row in rows),
         #     "New to-do item did not appear in table -- its text was:\n%s" % (
         #         table.text,
         #         )
         #     )
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+
+        self.assertIn('1 :Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows]
+        )
 
         self.fail('Finish the test!')
         # [...rest of comments as before]
