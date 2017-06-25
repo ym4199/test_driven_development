6c03b606b87964274f24fade110dfe84950435f1
ym4199 <ym4199@gmail.com>
2017-06-25 18:36:09 +0900

    ft error message return

--- functional_tests.py
+++ functional_tests.py
@@ -42,16 +42,19 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
+        # import time
         # time.sleep(10)
         # table = self.browser.find_element_by_id('id_list_table')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-            )
-        # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        # self.assertTrue(
+        #     any(row.text == '1:Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table -- its text was:\n%s" % (
+        #         table.text,
+        #         )
+        #     )
+        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
 
         self.fail('Finish the test!')
         # [...rest of comments as before]
