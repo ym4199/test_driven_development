8298c7a1ee35fdf4113ab0f04b6cd0524ef49ad3
ym4199 <ym4199@gmail.com>
2017-06-25 14:35:35 +0900

    리펙터링 작업 완료

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -23,3 +23,4 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-
-
 # Create your views here.
+from django.shortcuts import render
+
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
